17b10fb814f6047d0f7f4f9e8ac20945e3b02ae1
12 <wok191142@gmail.com>
2025-01-27 00:07:57 +0800

    work8

--- src/work8/Hw2Train.java
+++ src/work8/Hw2Train.java
@@ -2,9 +2,10 @@ package work8;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Set;
+import java.util.List;
 
 //請設計一個Train類別,並包含以下屬性:
 //- 班次 number,型別為int - 車種 type,型別為String - 出發地 start,型別為String
@@ -42,29 +43,40 @@ public class Hw2Train {
 		trainList.add(new Train(1254, "區間", "屏東", "基隆", 700));
 
 		// • 請寫一隻程式,能印出不重複的Train物件(以上三題請根據使用的集合,練習各種取值寫法,如迭代器、for迴圈或foreach等)
-
-		Set<Train> trainset = new HashSet<>(trainList);
-		Iterator<Train> it1 = trainset.iterator();
+		Collection<Train> trainSet = new HashSet<>(trainList);
+		Iterator<Train> it1 = trainSet.iterator();
 		while (it1.hasNext()) {
-
-			System.out.println(it1.equals(it1.next()));
-
+			System.out.println(it1.next().getNumber());
 		}
-
+		System.out.println("========================");
+		
 		// • 請寫一隻程式,讓Train物件印出時,能以班次編號由大到小印出(以上三題請根據使用的集合,練習各種取值寫法,如迭代器、for迴圈或foreach等)
-
+		Collections.sort((List) trainList);
+		Object[] a = trainList.toArray();
 		for (int i = 0; i < trainList.size(); i++) {
-			System.out.println(trainList.toArray());
+			System.out.println(((Train)a[i]).getNumber());
 		}
-
+		System.out.println("========================");
+		
 		// • 承上,不僅能讓班次編號由大排到小印出,
 		// 還可以不重複印出Train物件(以上三題請根據使用的集合,練習各種取值寫法,如迭代器、for迴圈或foreach等)
-
+		Collection<Train> trainSet2 = new HashSet<>(trainList);
+		Collection<Train> trainList2 = new ArrayList<>(trainSet2);
+		Collections.sort((List)trainList2);
+		Iterator<Train> it2 = trainList2.iterator();
+		while (it2.hasNext()) {
+			System.out.println(it2.next().getNumber());
+		}
+		System.out.println("========================");
+		
+		
+		
+		
 	}
 
 }
 
-class Train {
+class Train implements Comparable<Train>{
 
 	private int number; // 班次
 	private String type; // 車種
@@ -72,6 +84,8 @@ class Train {
 	private String dest; // 目的地
 	private double price; // 票價
 
+
+	
 	public Train() {
 		super();
 	}
@@ -107,11 +121,21 @@ class Train {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this.number == ((Train) obj).number)
+		if (this.number == ((Train) obj).number) {
 			return true;
-		else
+		} else {
 			return false;
+		}
 
 	}
 
+	@Override
+	public int hashCode() {
+		return number;
+	}
+
+    @Override
+    public int compareTo(Train t) {
+        return Integer.compare(this.number, t.number);
+    }
 }
